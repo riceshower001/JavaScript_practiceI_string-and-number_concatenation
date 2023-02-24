954209b229d5dfeff3f3a36ae999855137c53bc1
riceshower001 <125946802+riceshower001@users.noreply.github.com>
2023-02-25 04:33:16 +0900

    Add files via upload

---
+++ JavaScript_practiceI_string and number_concatenation.js
@@ -0,0 +1,5 @@
+// Please concatenate "Hello" and "JS" and output it to the console.
+console.log("Hello"+"JS");
+
+// Please concatenate the strings "20" and "15" and output them to the console.
+console.log("20"+"15");
